aaee5888604d8e927db06788318ffeb98872d52c
Damian Carballo <damiangcarballo@gmail.com>
2023-10-08 20:50:35 -0300

    PokemonQuest V1

--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-const carousel = document.querySelector(".carousel");
-const slides = document.querySelectorAll(".slide");
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-let currentIndex = 0;
-
-function showSlide(index) {
-    const slideWidth = 600;
-    carousel.style.transform = `translateX(-${index * slideWidth}px)`;
-}
-
-prevButton.addEventListener("click", () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        showSlide(currentIndex);
-    }
+$(document).ready(function () {
+    $('#theCarousel').carousel({
+        interval: 3000 // Cambia cada 3 segundos
+    });
 });
-
-nextButton.addEventListener("click", () => {
-    if (currentIndex < slides.length - 1) {
-        currentIndex++;
-        showSlide(currentIndex);
-    }
-});
-
-// Auto-avance (opcional)
-setInterval(() => {
-    if (currentIndex < slides.length - 1) {
-        currentIndex++;
-        showSlide(currentIndex);
-    } else {
-        currentIndex = 0;
-        showSlide(currentIndex);
-    }
-}, 3000);
