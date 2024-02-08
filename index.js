9c404191b475127e0c12257089a0b002c3495ab1
SoyDament <damiangcarballo@gmail.com>
2024-02-08 11:30:10 -0300

    Rework visual de la pagina - Test de adaptacion para celulares

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-$(document).ready(function () {
-    $('#theCarousel').carousel({
-        interval: 3000 // Cambia cada 3 segundos
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    const pokeImage = document.getElementById("pokemon-image");
+    let attempts = 0;
+
+    function startGame(generation) {
+        attempts = 0;
+        const optionsList = document.getElementById("options-list");
+        optionsList.classList.remove("disabled");
+
+        // Restaurar la visibilidad y la animación de la imagen
+        pokeImage.style.display = 'block';
+        pokeImage.style.animation = "reveal 2s ease-in-out";
+
+        setTimeout(() => {
+            getRandomPokemon(generation, (data) => {
+                pokeImage.src = data.pngUrl;
+                generateOptions(data.pokemonName, (options) => {
+                    displayOptions(options, data.pokemonName);
+                    // Quitar la animación después de mostrar las opciones
+                    pokeImage.style.animation = "none";
+                });
+            });
+        }, 2000);
+    }
+
+    // Resto del código...
 });
